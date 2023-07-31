b603890844d6ba9bc18635cd43b3b7153b278d43
chit ko <80560570+chitkoko7214084@users.noreply.github.com>
2023-07-31 18:47:54 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+#Password Generator 
+#Author Chit Ko ko
+
+
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+random_letters = [random.choice(letters) for _ in range(nr_letters)]
+random_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+random_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+#random_password=[random.choice(random_letters and random_symbols and random_symbols) for _ in range(nr_letters and nr_symbols and nr_numbers)]
+
+random_password= random_letters + random_symbols + random_numbers
+
+random.shuffle(random_password)
+
+password=''.join(random_password)
+
+print(password)
+
